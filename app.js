121ca7929b0edc90e5f8ef9f03d4f9faf4780dd7
rdthetha <rd10072000@gmail.com>
2021-09-29 22:50:34 +0530

    features

---
+++ app.js
@@ -0,0 +1,34 @@
+const initial = document.querySelector("#initial-price");
+const quantity = document.querySelector("#no-of-stocks");
+const final = document.querySelector("#current-price");
+const output = document.querySelector("#output-msg");
+const submitBtn = document.querySelector("#output-btn");
+
+submitBtn.addEventListener('click', function OutputHandler(){
+    output.style.display="none";
+	var initialPrice = Number(initial.value);
+	var finalPrice = Number(final.value);
+	var stocksQuantity = Number(quantity.value);
+
+	calculateProfitLoss(initialPrice, stocksQuantity, finalPrice);
+});
+
+function calculateProfitLoss(i, q, f) {
+    output.style.display="block";
+	if (i > f) {
+		let loss = (i - f) * q;
+		let lossPercent = (loss / (i * q)) * 100;
+		output.style.color = "red";
+		output.innerHTML = `Your Loss is ${loss} and loss Percentage is ${lossPercent.toFixed(2)}%`;
+	}
+	else if (i < f) {
+		let profit = (f - i) * q;
+		let profitPercent = (profit / (i * q)) * 100;
+		output.style.color = "green";
+		output.innerHTML = `Your Profit is ${profit} and profit Percentage is ${profitPercent.toFixed(2)}%`;
+	}
+	else {
+		output.style.color = "white";
+		output.innerHTML = `No pain No gain and no gain no pain!!`;
+	}
+}
